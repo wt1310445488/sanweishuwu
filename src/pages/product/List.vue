b8cfa557182401639a45f89cf573632d66d92cb0
wt1310445488 <1310445488@qq.com>
2020-01-05 23:16:57 +0800

    可以上传图片

--- src/pages/product/List.vue
+++ src/pages/product/List.vue
@@ -7,15 +7,17 @@
         <!-- 表格开始 -->
         <el-table :data="products">
         <el-table-column prop = "id" label="编号"></el-table-column>
-        <el-table-column prop="name" label="产品名称"></el-table-column>
+        <el-table-column prop="name" width="200px" label="产品名称"></el-table-column>
         <el-table-column prop="price" label="价格"></el-table-column>
-        <el-table-column prop="description" label="描述"></el-table-column>
+        <el-table-column prop="description" width="500px" label="描述"></el-table-column>
         <el-table-column prop="categoryId" label="所属栏目"></el-table-column>
-        <el-table-column label="操作">
+        <el-table-column prop="photo" width="650px" label="图片"></el-table-column>
+
+        <el-table-column label="操作" fixed="right">
             <template v-slot="slot">
                 <a href=""  @click.prevent="toDeleteHandler(slot.row.id)"><i class="el-icon-delete"></i></a>
                 <a href="" @click.prevent="toUpdateHandler(slot.row)"><i class="el-icon-edit-outline"></i></a>
-                <a href="" >详情</a>
+                
             </template>
         </el-table-column>
         </el-table>
@@ -43,7 +45,7 @@
                 </el-input>
             </el-form-item>
            
-             <el-form-item label="所属栏目">
+            <el-form-item label="所属栏目">
                 <el-select v-model="form.categoryId" placeholder="请选择">
                 <el-option
                 v-for="item in options"
@@ -53,9 +55,20 @@
                 </el-option>
             </el-select>
             </el-form-item>
-             <el-form-item label="描述">
+            <el-form-item label="描述">
                 <el-input type="textarea" v-model="form.description">
                 </el-input>
+              </el-form-item>
+            <el-form-item label="图片">
+                <el-upload
+                    class="upload-demo"
+                    action="http://134.175.154.93:6677/file/upload"
+                    :file-list="fileList"
+                    :on-success="uploadSuccessHandler"
+                    list-type="picture">
+                    <el-button size="small" type="primary">点击上传</el-button>
+                    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                </el-upload>
             </el-form-item>
           </el-form>
         <!-- <span>录入顾客信息信息</span> -->
@@ -73,6 +86,14 @@ import querystring from 'querystring'
 export default {
     //用于存放网页中需要调用的方法
     methods:{
+        //上传成功的事件处理
+        uploadSuccessHandler(response){
+            console.log(response);
+            let photo = "http://134.175.154.93:8888/group1/"+response.data.id
+             //将图片设置到form中便于一起提交给后台
+            this.form.photo = photo;
+            
+        },
         loadCategory(){
              let url = "http://localhost:6677/category/findAll"
         request.get(url).then((response)=>{
@@ -109,17 +130,19 @@ export default {
             })
         },
         toAddHandler(){
+            this.fileList = [];
             this.form = {
         // type:"product"
         }
-            this.title="录入员工信息";
+            this.title="录入产品信息";
             this.visible = true;
         },
         closeModalHandler(){
             this.visible = false;
         },
         toUpdateHandler(row){
-            this.title = "更新员工信息";
+          this.fileList = [];
+          this.title = "更新产品信息";
           this.visible = true;
           this.form = row;
         },
@@ -155,7 +178,8 @@ export default {
             products:[],
             form:{
                 // type:"product"
-            }
+            },
+            fileList:[]
         }
 
     },
